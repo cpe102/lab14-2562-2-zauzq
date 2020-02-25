c22aa7dc84489bc0a3e591fb990c12dfc11485bb
Mac <mac@Macs-MacBook-Pro.local>
2020-02-25 17:36:03 +0700

    done

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -1,11 +1,62 @@
-include <iostream>
+#include<iostream>
 using namespace std;
 
 
 int main(){
 	//Write your code here.
-	
-	
-	
+	int a = 5;
+	char b = 'A';
+	char &c = b;
+	int *x = &a;
+	char *y = &b;
+	int **z = &x;
+
+	cout << " a : " << a << endl;
+	cout << " b : " << b << endl;
+	cout << " c : " << c << endl;
+	cout << " d : " << x << endl;
+	cout << " e : " << (int*)y << endl;
+	cout << " f : " << z << endl;
+
+	cout << "&a : " << &a << endl;
+	cout << "&b : " << (int*)&b << endl;
+	cout << "&c : " << (int*)&c << endl;
+	cout << "&d : " << &x << endl;
+	cout << "&e : " << &y << endl;
+	cout << "&f : " << &z << endl;
+
+	c = 'F';
+	cout << " a : " << a << endl;
+	cout << " b : " << b << endl;
+	cout << " c : " << c << endl;
+	cout << " x : " << x << endl;
+	cout << " y : " << (int*)y << endl;
+	cout << " z : " << z << endl;
+
+	*y = 'W';
+	cout << " a : " << a << endl;
+	cout << " b : " << b << endl;
+	cout << " c : " << c << endl;
+	cout << " x : " << x << endl;
+	cout << " y : " << (int*)y << endl;
+	cout << " z : " << z << endl;
+
+	*x = 6;
+	cout << " a : " << a << endl;
+	cout << " b : " << b << endl;
+	cout << " c : " << c << endl;
+	cout << " x : " << x << endl;
+	cout << " y : " << (int*)y << endl;
+	cout << " z : " << z << endl;
+
+	**z = 7;
+	cout << " a : " << a << endl;
+	cout << " b : " << b << endl;
+	cout << " c : " << c << endl;
+	cout << " x : " << x << endl;
+	cout << " y : " << (int*)y << endl;
+	cout << " z : " << z << endl;
+
+
 	return 0;
 }
